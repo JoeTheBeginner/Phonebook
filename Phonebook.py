9e88c86a6b3ff919cf11305b7c2b7269605ad6f3
JoeTheBeginner <45625062+JoeTheBeginner@users.noreply.github.com>
2018-12-06 17:36:47 +0100

    Add files via upload

---
+++ Phonebook.py
@@ -0,0 +1,33 @@
+class Phonebook :
+    def __init__(self):
+        self.__enteries = {}
+
+    def adder(self,*args):
+        m = [*args]
+        listestr = []
+        listeint = []
+        for n in m:
+            if type (n) == str:
+                listestr.append (n)
+            if type (n) == int:
+                listeint.append (n)
+            self.__enteries = dict (zip (listestr, listeint))
+        return self.__enteries
+
+    def searcher(self, search_name):
+        if search_name in self.__enteries:
+            return self.__enteries[search_name]
+        else:
+            return 'not found'
+    def __str__(self) :
+        return  'Phonebook' + str(self.__enteries) + '!'
+    def __repr__(self) :
+        return  self.__str__ ()
+    def len (self) :
+        return len(self.__enteries)
+   
+newyork = Phonebook ()
+newyork.adder ('youssef',2323445,'marlin',348275284,'müller',425745)
+print(newyork.searcher ('marlin'))
+print(newyork.__repr__())
+print(newyork.len())
